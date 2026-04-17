8c0a9e6af854000b82de4c22f82ced402acb0e15
Maryam Fatima <maryam@example.com>
2026-04-17 18:51:52 +0500

    Fix: Use PostgreSQL via DATABASE_URL

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -58,11 +58,14 @@ CHANNEL_LAYERS = {
     }
 }
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
 }
 
 AUTH_PASSWORD_VALIDATORS = []
@@ -119,3 +122,4 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
 
 
 # Force redeploy 
+
